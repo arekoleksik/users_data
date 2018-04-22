684845c2aba480d824ff7e3e3f260d2aaebdda29
Arek Oleksik <arkadiuszoleksik@gmail.com>
2018-04-22 08:41:04 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var peopleDB = [];
+function Person(attributes) {
+    this.name = attributes.name;
+    this.surname = attribustes.surname;
+    this.age = attributes.age;
+    this.role= attributes.role;
+}
